deaa63e9e79a455edbbe86d26afb87e5828bcd80
Anil Sonone <anilsonone@gmail.com>
2026-03-27 20:57:16 +0530

    Menu optimisaction

--- src/components/Dashboard2.tsx
+++ src/components/Dashboard2.tsx
@@ -1,57 +1,67 @@
-import { Box, Button, Flex, Grid, GridItem, Heading, HStack, List, ListItem, Spacer, Text } from "@chakra-ui/react"
-import { AiOutlineDashboard, AiOutlineTeam, AiOutlineUser } from "react-icons/ai"
-import { MdOutlineClass, MdWeb } from "react-icons/md"
-import { FaChalkboardTeacher, FaSchool } from "react-icons/fa"
-import { NavLink, Outlet, useNavigate } from "react-router-dom"
-import { IconButton, useDisclosure } from "@chakra-ui/react";
+import {
+    Box, Button, Flex, Grid, GridItem, Heading, HStack,
+    List, ListItem, Spacer, Text, IconButton, useDisclosure
+} from "@chakra-ui/react";
+import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import {
     Drawer, DrawerOverlay, DrawerContent,
     DrawerBody, DrawerHeader
 } from "@chakra-ui/react";
-import { getLoggedinUserEmailId } from "../service/ApiClient"
-import { getMenuByRoles } from "../types/SideMenuData"
+import { getLoggedinUserEmailId } from "../service/ApiClient";
+import { getMenuByRoles } from "../types/SideMenuData";
+
 const Dashboard2 = () => {
 
-    // Get user info from localStorage
     const loggedInUser = getLoggedinUserEmailId();
-
-
-
-
     const navItems = getMenuByRoles();
     const navigate = useNavigate();
+    const { isOpen, onToggle } = useDisclosure();
+
     const handleLogout = () => {
-        sessionStorage.removeItem("jwtToken");
         sessionStorage.clear();
         navigate("/");
-    }
-    const { isOpen, onToggle } = useDisclosure();
-    return (
+    };
 
+    return (
         <Grid templateColumns={{ base: "1fr", md: "250px 1fr" }}>
+
+            {/* Sidebar */}
             <GridItem
                 as="aside"
                 display={{ base: "none", md: "block" }}
                 bgGradient="linear(to-b, red.100, blue.300, green.500)"
                 p="20px"
             >
+                <List spacing={3} fontSize="18px">
+                    {navItems.map((item, index) => {
+                        const Icon = item.icon;
 
-                <List spacing={3} fontSize="20px">
-                    {navItems.map((item, index) => (<ListItem key={index}>
-                        <NavLink to={item.path} style={({ isActive }) => ({
-                            display: "flex",
-                            alignItems: "center", gap: "5px",
-                            color: isActive ? "rgb(248, 7, 7)" : "#031652ff",
-                            // fontWeight: isActive ? "bold" : "normal",
-                            textDecoration: isActive ? "underline" : "none",
-                        })}>{item.icon}{item.label}
-                        </NavLink>
-                    </ListItem>))}
-
+                        return (
+                            <ListItem key={index}>
+                                <NavLink
+                                    to={item.path}
+                                    style={({ isActive }) => ({
+                                        display: "flex",
+                                        alignItems: "center",
+                                        gap: "10px",
+                                        color: isActive ? "rgb(248, 7, 7)" : "#031652ff",
+                                        textDecoration: isActive ? "underline" : "none",
+                                    })}
+                                >
+                                    <Icon />
+                                    {item.label}
+                                </NavLink>
+                            </ListItem>
+                        );
+                    })}
                 </List>
             </GridItem>
-            <GridItem as="main" >
+
+            {/* Main Content */}
+            <GridItem as="main">
+
+                {/* Top Navbar */}
                 <Flex
                     as="nav"
                     p="10px"
@@ -59,7 +69,6 @@ const Dashboard2 = () => {
                     bgGradient="linear(to-r, indigo, purple.500, pink.500)"
                     color="white"
                 >
-                    {/* Mobile menu button */}
                     <IconButton
                         icon={<HamburgerIcon />}
                         display={{ base: "block", md: "none" }}
@@ -75,7 +84,9 @@ const Dashboard2 = () => {
                     <Spacer />
 
                     <HStack spacing={3}>
-                        <Box fontSize="20px" bg="green.400">SP</Box>
+                        <Box fontSize="20px" bg="green.400" px={2} borderRadius="md">
+                            SP
+                        </Box>
                         <Text display={{ base: "none", md: "block" }}>
                             {loggedInUser || "User"}
                         </Text>
@@ -84,33 +95,47 @@ const Dashboard2 = () => {
                         </Button>
                     </HStack>
                 </Flex>
+
+                {/* Mobile Drawer */}
                 <Drawer isOpen={isOpen} placement="left" onClose={onToggle}>
                     <DrawerOverlay />
                     <DrawerContent>
                         <DrawerHeader>Menu</DrawerHeader>
                         <DrawerBody>
                             <List spacing={3}>
-                                {navItems.map((item, index) => (
-                                    <ListItem key={index}>
-                                        <NavLink to={item.path} onClick={onToggle}>
-                                            {item.icon} {item.lable}
-                                        </NavLink>
-                                    </ListItem>
-                                ))}
+                                {navItems.map((item, index) => {
+                                    const Icon = item.icon;
+
+                                    return (
+                                        <ListItem key={index}>
+                                            <NavLink
+                                                to={item.path}
+                                                onClick={onToggle}
+                                                style={{
+                                                    display: "flex",
+                                                    alignItems: "center",
+                                                    gap: "10px",
+                                                }}
+                                            >
+                                                <Icon />
+                                                {item.label}
+                                            </NavLink>
+                                        </ListItem>
+                                    );
+                                })}
                             </List>
                         </DrawerBody>
                     </DrawerContent>
                 </Drawer>
+
+                {/* Page Content */}
                 <Box m={{ base: "10px", md: "20px" }}>
                     <Outlet />
                 </Box>
 
             </GridItem>
-
-
-
         </Grid>
-    )
-}
+    );
+};
 
-export default Dashboard2
+export default Dashboard2;
